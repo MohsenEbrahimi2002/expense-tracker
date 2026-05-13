dafbe58280804c89b0889dcba778789be02ce519
Mohsen Ebrahimi <mohsen.eb1381@gmail.com>
2026-05-13 14:55:39 +0330

    Add useUserAuth custom hook to Home component

--- frontend/src/pages/Dashboard/Home.tsx
+++ frontend/src/pages/Dashboard/Home.tsx
@@ -1,11 +1,13 @@
-import DashboardLayout from "../../components/layout/DashboardLayout"
+import DashboardLayout from "../../components/layout/DashboardLayout";
+import { useUserAuth } from "../../hooks/useUserAuth";
 
 function Home() {
+  useUserAuth();
   return (
     <DashboardLayout activeMenu="Dashboard">
-    <div className="my-5 mx-auto">Home</div>
+      <div className="my-5 mx-auto">Home</div>
     </DashboardLayout>
-  )
+  );
 }
 
-export default Home
+export default Home;
